a79992194e49a2fe595c4a3c094c7c733186edf5
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2023-11-13 03:57:41 +0200

    js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './js/modal.js';
+import './js/best-sellers.js';
+import './js/scrollBtn.js';
+import './js/category-list.js';
+import './js/api-requests.js';
+import './js/header.js';
+import './js/dark-theme.js';
